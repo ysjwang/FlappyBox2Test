4ca6a8ae536cc4a9c7ae92fb2f5d86f08fc86142
Josiah <ysjwang@gmail.com>
2021-11-28 22:18:13 -0500

    Added some flapping

---
+++ Assets/FlappyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyController : MonoBehaviour
+{
+    public float upForce;
+    
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Pressed Space
+            rigidBody.AddForce(new Vector3(0, upForce, 0));
+            Debug.Log("Pressed Space");
+        }
+    }
+}
