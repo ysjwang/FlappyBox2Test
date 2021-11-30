07565fcf5d0cf84f12a8117009f8fc5990cdf35d
Josiah <ysjwang@gmail.com>
2021-11-30 07:48:01 -0500

    Added ceiling spawning

--- Assets/ObstacleSet.cs
+++ Assets/ObstacleSet.cs
@@ -7,21 +7,33 @@ public class ObstacleSet : MonoBehaviour
 
     public float xSpeed;
     public GameObject floorPlateAttach;
+    public GameObject ceilingPlateAttach;
     public List<GameObject> floorPlates;
+    public List<GameObject> ceilingPlates;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject floorPlate = GameObject.Instantiate(PickRandomFloorPlate());
+        GameObject floorPlate = GameObject.Instantiate(RandomFloorPlate());
         floorPlate.transform.parent = transform;
         floorPlate.transform.position = floorPlateAttach.transform.position;
 
+        GameObject ceilingPlate = GameObject.Instantiate(RandomCeilingPlate());
+        ceilingPlate.transform.parent = transform;
+        ceilingPlate.transform.position = ceilingPlateAttach.transform.position;
+
     }
 
-    GameObject PickRandomFloorPlate()
+    GameObject RandomFloorPlate()
     {
         int index = Random.Range(0, floorPlates.Count);
         return floorPlates[index];
+    }
+
+    GameObject RandomCeilingPlate()
+    {
+        int index = Random.Range(0, ceilingPlates.Count);
+        return ceilingPlates[index];
     }
 
     // Update is called once per frame
